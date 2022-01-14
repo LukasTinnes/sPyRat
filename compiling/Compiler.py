64002a7a7a67ae90c3cd152d61ee9df61e5b0014
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-01-15 00:43:19 +0100

    Updated docs

--- compiling/Compiler.py
+++ compiling/Compiler.py
@@ -6,11 +6,12 @@ class Compiler:
     Deals with  Writing to files using Information gained from crawling
     """
 
-    def decompile(self, data: DataFrame, lod_file:str, save_file_path:str, prefix:str):
+    def decompile(self, data: DataFrame, load_file:str, save_file_path:str, prefix:str):
         """
         Saves data given in data frames as a new file each to the given file_path.
         :param data: The dataframe of the crawled files. Expects a format (start byte, end byte, size, confidence, file_type)
-        :param file_path: The path to where the file should be saved.
+        :param save_file_path: The path to where the file should be saved.
+        :param load_file: The file path for the file which contents should be decompiled
         :param prefix: The prefix of the file to be saved.
         :return:
         """

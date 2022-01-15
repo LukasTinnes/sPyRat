d0c8afe03dc5822a28118904340c7c4904f6d501
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-01-15 12:04:58 +0100

    RAT-30 Added skeleton for BMP crawler

--- crawling/crawlers/file_crawler.py
+++ crawling/crawlers/file_crawler.py
@@ -1,6 +1,7 @@
 from crawling.crawler import Crawler
 from crawling.crawler_data_structures.crawl_data import CrawlData
 from abc import abstractmethod
+import os
 
 
 class FileCrawler(Crawler):
@@ -15,4 +16,14 @@ class FileCrawler(Crawler):
         :param file:
         :return:
         """
-        pass
+        file_size_in_bytes = os.path.getsize(file)
+        with open(file, "rb") as f:
+            for i in range(file_size_in_bytes):
+                f.seek(i)
+                header_bytes = f.read(2)
+                header_field_string = header_bytes.decode("ASCII")
+                if header_field_string in ["BM", "BA", "CI", "CP", "IC", "PT"]:
+                    self._crawl_at_byte(file, i, header_field_string)
+
+    def _crawl_at_byte(self, file, index, header_field_string):
+        ...
---
+++ crawling/crawlers/file_crawlers/bmp_crawler.py
@@ -0,0 +1,12 @@
+class BMPCrawler:
+
+    def __init__(self):
+        ...
+
+    def crawl(self, file: str) -> CrawlData:
+        """
+        Crawls through a file to find a file pattern at each byte.
+        :param file:
+        :return:
+        """
+        pass

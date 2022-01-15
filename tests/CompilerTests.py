af206c6c8548bfbc79927df6cd081a2737b357ae
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-01-15 11:31:47 +0100

    RAT-25 Added extra test

--- tests/CompilerTests.py
+++ tests/CompilerTests.py
@@ -15,3 +15,14 @@ class CompilerTests(TestCase):
         with open(save_file_path + prefix + "_0.txt") as file:
             export = file.read(10)
             self.assertEqual(export, "ABCDEFGHIJ")
+
+    def test_bytes(self):
+        data = DataFrame({"start_byte": [0], "end_byte": [9], "size": [10], "file_type": "bytes"})
+        load_file = "files\\test_bytes"
+        save_file_path = "output\\"
+        prefix = "test"
+        Compiler.decompile(data, load_file, save_file_path, prefix)
+
+        with open(save_file_path + prefix + "_0.bytes","rb") as file:
+            export = file.read(10)
+            self.assertEqual(export, bytes(list(range(10))))

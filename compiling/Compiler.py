985a1f5956246302812ff5b9617cbd8a2d54a03c
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-01-15 01:17:42 +0100

    RAT-25 Added Implementation and a test for decompile

--- compiling/Compiler.py
+++ compiling/Compiler.py
@@ -6,7 +6,8 @@ class Compiler:
     Deals with  Writing to files using Information gained from crawling
     """
 
-    def decompile(self, data: DataFrame, load_file:str, save_file_path:str, prefix:str):
+    @staticmethod
+    def decompile(data: DataFrame, load_file: str, save_file_path: str, prefix: str):
         """
         Saves data given in data frames as a new file each to the given file_path.
         :param data: The dataframe of the crawled files. Expects a format (start byte, end byte, size, confidence, file_type)
@@ -15,4 +16,12 @@ class Compiler:
         :param prefix: The prefix of the file to be saved.
         :return:
         """
-        ...
+        with open(load_file, 'rb') as load_file:
+            for index, row in data.iterrows():
+                start_byte = row["start_byte"]
+                size = row["size"]
+                file_type = row["file_type"]
+                load_file.seek(start_byte)
+                bytes = load_file.read(size)
+                with open(f"{save_file_path}\\{prefix}_{index}.{file_type}", "wb") as save_file:
+                    save_file.write(bytes)
---
+++ tests/CompilerTests.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from compiling.Compiler import Compiler
+from pandas import DataFrame
+
+
+class CompilerTests(TestCase):
+
+    def test_txt(self):
+        data = DataFrame({"start_byte": [0], "end_byte": [9], "size": [10], "file_type": "txt"})
+        load_file = "files\\test.txt"
+        save_file_path = "output\\"
+        prefix = "test"
+        Compiler.decompile(data, load_file, save_file_path, prefix)
+
+        with open(save_file_path + prefix + "_0.txt") as file:
+            export = file.read(10)
+            self.assertEqual(export, "ABCDEFGHIJ")

e2d8498c9c9de2b7b688a7963c7e2cd9f6abf3d1
Lukers <lukas@yee.com>
2022-01-16 12:32:49 +0100

    RAT-26 Mild Refactoring, general structure

--- crawling/crawlers/file_crawlers/png_crawler.py
+++ crawling/crawlers/file_crawlers/png_crawler.py
@@ -5,30 +5,57 @@ from pandas import DataFrame
 from multiprocessing import Pool
 
 
+def get_max_size_of_file(file):
+    return os.path.getsize(file)
+
+
+def signature_is_valid(signature, carriage_return, line_feed, text_close, line_feed_2, zero_bytes):
+    if signature != "PNG":
+        return False
+    if carriage_return != b'\r' or line_feed == b'\n':
+        return False
+
+
+
 class JPGCrawler(FileCrawler):
 
+    POOL_SIZE = 4
     HEADER_SIZE = 11
 
     def __init__(self):
         self.MAX_FILE_SIZE = 0
         self.bytes = []
+        self.rows = []
 
     def crawl(self, file: str) -> CrawlData:
-        self.MAX_FILE_SIZE = os.path.getsize(file)
-        with open(file, "rb") as f:
-            self.bytes = f.read()
-        with Pool(4) as pool:
-            pool.starmap(self.check_byte, range(self.MAX_FILE_SIZE - 8))
+        self.MAX_FILE_SIZE = get_max_size_of_file(file)
+        self.read_byte_stream(file)
+        self.crawl_by_pools()
 
-    def check_byte(self, index):
-        if index + self.HEADER_SIZE < self.MAX_FILE_SIZE:
-            byte_01, file_signature, carriage_return, line_feed, byte_07, parity, zero_bytes = self.parse_header(index)
+    def check_bytes(self, indices):
+        for index in indices:
+            if self.header_is_in_bounds(index):
+                signature, carriage_return, line_feed, text_close, line_feed_2, zero_bytes = self.parse_header(index)
+                if signature_is_valid(signature, carriage_return, line_feed, text_close, line_feed_2, zero_bytes):
+                    self.add_row(index)
+
+    def header_is_in_bounds(self, index):
+        return index + self.HEADER_SIZE < self.MAX_FILE_SIZE
 
     def parse_header(self, index):
         # header = self.bytes[]
         pass
 
+    def read_byte_stream(self, file):
+        with open(file, "rb") as file_stream:
+            self.bytes = bytes(file_stream.read())
+
+    def crawl_by_pools(self):
+        with Pool(self.POOL_SIZE) as pool:
+            pool.starmap(self.check_bytes, range(self.MAX_FILE_SIZE - 8))
+
+
 if __name__ == "__main__":
-    with open("C:\\Users\\Lukers\\Documents\\fouroclocks.png", "rb") as f:
-        data = f.read()
-        print(data)
+    with open("C:\\Users\\Lukers\\Documents\\Zaunfreier Doggo.png", "rb") as f:
+        for x in range(11):
+            print(f.read(1))

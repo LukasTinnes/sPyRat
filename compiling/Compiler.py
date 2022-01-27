cc0640baeff79ee664fd7e87baa19885488bdab0
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-01-27 23:58:01 +0100

    A small addendum to the compiler. Now you can have lots of outputs in the same file

--- compiling/Compiler.py
+++ compiling/Compiler.py
@@ -25,3 +25,20 @@ class Compiler:
                 bytes = load_file.read(size)
                 with open(f"{save_file_path}\\{prefix}_{index}.{file_type}", "wb") as save_file:
                     save_file.write(bytes)
+
+    @staticmethod
+    def decompile_to_single_file(data: DataFrame, load_file: str, save_file_path: str, prefix: str, seperation=None):
+        if seperation is None:
+            seperation = bytearray()
+        with open(load_file, 'rb') as load_file:
+            file_types = data["file_type"].unique()
+            for file_type in file_types:
+                with open(f"{save_file_path}\\{prefix}_all.{file_type}", "wb") as save_file:
+                    for index, row in data.iterrows():
+                        start_byte = row["start_byte"]
+                        size = row["size"]
+                        load_file.seek(start_byte)
+                        bytes = load_file.read(size)
+                        save_file.write(bytes)
+                        save_file.write(seperation)
+

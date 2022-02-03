234a2342f2ca3603c28f7da765c92a79c6188395
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-02-03 21:28:03 +0100

    RAT-30 Added returns more complient to linter

--- crawling/crawlers/file_crawler.py
+++ crawling/crawlers/file_crawler.py
@@ -16,3 +16,24 @@ class FileCrawler(Crawler):
         :return:
         """
         pass
+
+    @abstractmethod
+    def crawl_in_range(self, file: str, start_byte: int, end_byte: int) -> CrawlData:
+        """
+        Crawls the file for a file pattern between the start byte (inclusive) and the end_byte (exclusive).
+        :param file: Tjhe file path
+        :param start_byte: The byte to start crawling at.
+        :param end_byte: The byte to end crawling at.
+        :return:
+        """
+        ...
+
+    @abstractmethod
+    def crawl_at_byte(self, file:str, start_byte: int = 0) -> CrawlData:
+        """
+        Crawls for a file pattern at the specific byte given.
+        :param file: The filepath.
+        :param start_byte: The byte to start crawling at.
+        :return:
+        """
+        ...

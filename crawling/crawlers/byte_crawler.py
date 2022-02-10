d3d0f4bd855069a4fe6180748bf9bb1ecca6ce23
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2022-02-10 01:02:36 +0100

    RAT-36 Added General byte crawler the ASCII crawler can just use.

--- crawling/crawlers/byte_crawler.py
+++ crawling/crawlers/byte_crawler.py
@@ -6,10 +6,13 @@ from multiprocessing.pool import Pool
 import os
 import math
 
-class TxtAsciiCrawler(FileCrawler):
 
-    def __init__(self, min_size=0):
+class ByteCrawler(FileCrawler):
+
+    def __init__(self, min_size=0, elements=None, pattern="byte"):
         self.min_size = min_size
+        self.elements = [] if elements is None else elements
+        self.pattern = pattern
 
     POOLS = 4
 
@@ -23,8 +26,8 @@ class TxtAsciiCrawler(FileCrawler):
         rows = self.crawl_range((0, file_size_in_bytes, file))
 
         df = DataFrame(rows)
-        df.rename(columns={0:"start_byte", 1:"end_byte", 2: "size", 3: "confidence"}, inplace=True)
-        return CrawlData(df, "ascii.txt") # TODO pattern s tring
+        df.rename(columns={0: "start_byte", 1: "end_byte", 2: "size", 3: "confidence"}, inplace=True)
+        return CrawlData(df, self.pattern)
 
     def crawl_range(self, args):
         start = args[0]
@@ -39,7 +42,7 @@ class TxtAsciiCrawler(FileCrawler):
             for i in range(start, end):
                 textbyte = f.read(1)
                 val = int.from_bytes(textbyte, "little", signed=False)
-                if val in range(32, 127) or val in [9, 10, 11, 13]:
+                if val in self.elements:
                     if not read_state:
                         read_byte = i
                         read_state = True
@@ -48,5 +51,6 @@ class TxtAsciiCrawler(FileCrawler):
                         rows.append([read_byte, i, i-read_byte, ((127-32+4)/256)**(i-read_byte)])
                         read_state = False
             if read_state:
-                rows.append([read_byte, end-1, end-1 - read_byte, math.log(((127 - 32 + 4) / 256) ** (end-1 - read_byte),2)])
+                confidence =  math.log((len(self.elements) / 256) ** (end-1 - read_byte), 2)
+                rows.append([read_byte, end-1, end-1 - read_byte, confidence])
         return rows
